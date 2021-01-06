e940f2de36f42a85b2631b370d2a8f156a0a5a60
hedixia <xiahedi@gmail.com>
2021-01-06 10:26:03 -0800

    --

--- mnist_anode.py
+++ mnist_anode.py
@@ -17,29 +17,23 @@ args = parser.parse_args()
 
 # shape: [time, batch, derivatives, channel, x, y]
 dim = 1
-hidden = 80
-trdat, tsdat = mnist(batch_size=256)
+hidden = 60
+aug=5
+trdat, tsdat = mnist()
 
 
 class initial_velocity(nn.Module):
 
-    def __init__(self, in_channels, out_channels, nhidden):
+    def __init__(self, in_channels, aug):
         super(initial_velocity, self).__init__()
-        assert (out_channels >= in_channels)
-        self.tanh = nn.Hardtanh(min_val=-5, max_val=5, inplace=False)
-        self.fc1 = nn.Conv2d(in_channels, nhidden, kernel_size=1, padding=0)
-        self.fc2 = nn.Conv2d(nhidden, nhidden, kernel_size=3, padding=1)
-        self.fc3 = nn.Conv2d(nhidden, 3 * out_channels - in_channels, kernel_size=1, padding=0)
+        self.aug = aug
 
     def forward(self, x0):
-        x0 = x0.float()
-        out = self.fc1(x0)
-        out = self.tanh(out)
-        out = self.fc2(out)
-        out = self.tanh(out)
-        out = self.fc3(out)
-        out = torch.cat([x0, out], dim=1)
-        out = rearrange(out, 'b (d c) x y -> b d c x y', d=3)
+        x0 = rearrange(x0.float(), 'b c x y -> b 1 c x y')
+        outshape = list(x0.shape)
+        outshape[1] = self.aug
+        out = torch.zeros(outshape)
+        out[:,1] += x0
         return out
 
 
@@ -48,20 +42,16 @@ class DF(nn.Module):
     def __init__(self, in_channels, nhidden):
         super(DF, self).__init__()
         self.activation = nn.ReLU()
-        self.fc1 = nn.Conv2d(in_channels + 1, nhidden, kernel_size=1, padding=0)
-        self.fc2 = nn.Conv2d(nhidden + 1, nhidden, kernel_size=3, padding=1)
-        self.fc3 = nn.Conv2d(nhidden + 1, in_channels, kernel_size=1, padding=0)
+        self.fc1 = nn.Conv2d(in_channels, nhidden, kernel_size=1, padding=0)
+        self.fc2 = nn.Conv2d(nhidden, nhidden, kernel_size=3, padding=1)
+        self.fc3 = nn.Conv2d(nhidden, in_channels, kernel_size=1, padding=0)
 
     def forward(self, t, x0):
-        out = rearrange(x0, 'b 1 c x y -> b c x y')
-        t_img = torch.ones_like(x[:, :1, :, :]).to(args.gpu) * t
-        out = torch.cat([out, t_img], dim=1)
-        out = self.fc1(out)
+        x0 = rearrange(x0, 'b 1 c x y -> b c x y')
+        out = self.fc1(x0)
         out = self.activation(out)
-        out = torch.cat([out, t_img], dim=1)
         out = self.fc2(out)
         out = self.activation(out)
-        out = torch.cat([out, t_img], dim=1)
         out = self.fc3(out)
         out = rearrange(out, 'b c x y -> b 1 c x y')
         return out
@@ -79,11 +69,11 @@ class predictionlayer(nn.Module):
 
 
 gamma = 0.0  # nn.Parameter(torch.tensor([0.0]))
-hblayer = NODElayer(HeavyBallODE(DF(dim, dim), gamma))
-model = nn.Sequential(initial_velocity(dim, dim, hidden), hblayer, predictionlayer(dim), nn.Softmax(dim=1)).to(device=args.gpu)
+anlayer = NODElayer(NODE(DF(dim, dim)))
+model = nn.Sequential(initial_velocity(dim, aug), anlayer, predictionlayer(dim), nn.Softmax(dim=1))
 
-optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.00)
-loss_func = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=0.0001)
+loss_func = nn.MSELoss()
 
 itr_arr = np.empty(args.niters)
 loss_arr = np.empty(args.niters)
@@ -94,7 +84,7 @@ itrcnt = 0
 # training
 start_time = time.time()
 min_loss = 1.0  # set arbitrary loss
-labeldefault = repeat(torch.arange(10), 'c -> b c', b=256)
+labeldefault = repeat(torch.arange(10), 'c -> b c', b=64)
 epoch = 0
 while epoch < args.niters:
     epoch += 1
@@ -105,11 +95,11 @@ while epoch < args.niters:
         optimizer.zero_grad()
         # forward in time and solve ode
         pred_y = model(x)
-
+        y = (repeat(y, 'b -> b c', c=10) == labeldefault[:y.shape[0]]).float()
         # compute loss
         loss = loss_func(pred_y, y)
         loss.backward()
-        nn.utils.clip_grad_norm_(model.parameters(), 10.0)
+        nn.utils.clip_grad_norm_(model.parameters(), 1.0)
         optimizer.step()
         # make arrays
         itr_arr[epoch - 1] = epoch
@@ -137,6 +127,7 @@ while epoch < args.niters:
             pred_y = model(x)
             pred_l = torch.argmax(pred_y, dim=1)
             acc += torch.sum((pred_l == y).float())
+            y = (repeat(y, 'b -> b c', c=10) == labeldefault[:y.shape[0]]).float()
             bcnt += 1
             # compute loss
             loss += loss_func(pred_y, y) * y.shape[0]
